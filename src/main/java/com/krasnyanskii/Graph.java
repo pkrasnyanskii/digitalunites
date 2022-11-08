0fdfd9a2683dd8df8c6ed186b5da8738407e04c3
pkrasnyanskii <p.krasnyanskii@g.nsu.ru>
2022-11-08 23:42:28 +0700

    Graph Task Repo

---
+++ src/main/java/com/krasnyanskii/Graph.java
@@ -0,0 +1,21 @@
+package com.krasnyanskii;
+
+public class Graph {
+
+    public class Vertex {
+        private Object vertex;
+        public Vertex(){
+            vertex = null;
+        }
+
+        public Object getVertex() {
+            return vertex;
+        }
+
+        public void setVertex(Object vertex){
+            this.vertex = vertex;
+        }
+    }
+
+
+}
---
+++ src/test/java/com/krasnyanskii/GraphTest.java
@@ -0,0 +1,4 @@
+package com.krasnyanskii;
+
+public class GraphTest {
+}
