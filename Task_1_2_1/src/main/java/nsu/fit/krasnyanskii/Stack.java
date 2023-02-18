2d0d87b31c4685c00e4bfa618d168006e122fc73
pkrasnyanskii <p.krasnyanskii@g.nsu.ru>
2023-02-18 23:46:37 +0700

    Comments

--- Task_1_2_1/src/main/java/nsu/fit/krasnyanskii/Stack.java
+++ Task_1_2_1/src/main/java/nsu/fit/krasnyanskii/Stack.java
@@ -1,7 +1,7 @@
 package nsu.fit.krasnyanskii;
 
-/** Stack class implements stack data structure
- *
+/**
+ * Stack class implements stack data structure
  * @param <T> Array of given data type
  */
 public class Stack<T> {
@@ -15,10 +15,18 @@ public class Stack<T> {
         top = -1;
     }
 
+    /**
+     * Returns current stack size
+     * @return is a stack size
+     */
     public int count() {
         return top + 1;
     }
 
+    /**
+     * Pushes an element to the stack
+     * @param x is a value of element
+     */
     public void push(T x){
         if (top == capacity - 1) {
             System.out.println("Stack OverFlow");
@@ -27,6 +35,10 @@ public class Stack<T> {
         arr[++top] = x;
     }
 
+    /**
+     * Pushes an array of elements to the stack
+     * @param list is an array of elements
+     */
     public void pushStack(T[] list) {
         if (list.length + count() > capacity) {
             System.out.println("Stack OverFlow");
@@ -37,6 +49,10 @@ public class Stack<T> {
         }
     }
 
+    /**
+     * Pops top element from the stack and returns new top element value
+     * @return - value of the element
+     */
     public T pop() {
         if (top == -1) {
             System.out.println("Stack empty");
@@ -45,6 +61,11 @@ public class Stack<T> {
         return arr[top--];
     }
 
+    /**
+     * Pops an array of elements from the stack with size n
+     * @param n - size of array
+     * @return - returns current top element value
+     */
     public T popStack(int n) {
         if (count() - n < 0) {
             System.out.println("Number of elements to pop is greater or equals to number of stack values");
